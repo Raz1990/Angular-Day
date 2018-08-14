631b8e0bec6e5b6f25bf48afa1ba66847509d5b8
Fulls7 <Fulls7@sqlabs.co.il>
2018-08-14 16:19:55 +0300

    end of day

---
+++ MyHeroAcademia/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './Classess/Hero';
+
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'All Might' },
+    { id: 12, name: 'Deku' },
+    { id: 13, name: 'Froppy' },
+    { id: 14, name: 'Saitama' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
